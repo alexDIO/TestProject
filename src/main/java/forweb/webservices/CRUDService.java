808af5fd4fd236caee6855104d8f36b3bf9dce78
Alex.DIO <olomakovskyi@dio-soft.com>
2014-11-06 17:24:42 +0200

    Implemented logging to CRUDService

--- src/main/java/forweb/webservices/CRUDService.java
+++ src/main/java/forweb/webservices/CRUDService.java
@@ -1,6 +1,7 @@
 package forweb.webservices;
 
 import forweb.TransportMapHolder;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import transport.classes.PassengerTransport;
@@ -21,6 +22,8 @@ import java.util.*;
 @Path("/")
 public class CRUDService {
 
+    final static Logger logger = Logger.getLogger(CRUDService.class);
+
     @Autowired
     private TransportMapHolder mapHolder;
 
@@ -29,9 +32,13 @@ public class CRUDService {
     @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
     public Map<Integer, Transport> getMap() throws IOException {
+        logger.info("Requested all transports");
         if (mapHolder != null) {
-            return mapHolder.getMap();
+            Map<Integer, Transport> resultMap = mapHolder.getMap();
+            logger.info(String.format("Was got map: %s", resultMap));
+            return resultMap;
         } else {
+            logger.error("Returned empty map.");
             return Collections.EMPTY_MAP;
         }
     }
@@ -40,18 +47,30 @@ public class CRUDService {
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Transport getTransportById(@PathParam("id") int id) {
-        return mapHolder.getMap().get(id);
+        logger.info(String.format("Requested transport with id %s", id));
+        Transport resultTransport = mapHolder.getMap().get(id);
+        if (resultTransport != null) {
+            logger.info(String.format("Was got transport: %s", resultTransport));
+            return mapHolder.getMap().get(id);
+        } else {
+            logger.error(String.format("Transport with id %s not found", id));
+            return null;
+        }
     }
 
     @GET
     @Path("/from_{from}_to_{to}")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Transport> getTransportByPassengerCount(@PathParam("from") int from, @PathParam("to") int to) {
+        logger.info(String.format("Requested transports with passenger count from %s to %s.", from, to));
         List<Transport> resultList = new ArrayList<>();
         Map<Integer, Transport> storage = mapHolder.getMap();
         for (Transport storedTransport : storage.values()) {
             if (storedTransport instanceof PassengerTransport && ((PassengerTransport) storedTransport).getPassengersCount() >= from && ((PassengerTransport) storedTransport).getPassengersCount() <= to) {
+                logger.info(String.format("Following transport fits criteria: %s", storedTransport));
                 resultList.add(storedTransport);
+            } else {
+                logger.warn(String.format("Following transport doesn't fit criteria: %s", storedTransport));
             }
         }
         return resultList;
